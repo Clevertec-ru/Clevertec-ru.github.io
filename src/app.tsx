b197b65255413d44b9ec056f166067f6f91b217c
Иванова Татьяна Игоревна <TIIvanova@alfabank.ru>
2024-11-01 15:52:42 +0300

    feat(*): add vite and start page

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import { StartPage } from './pages/start-page';
+
 export const App = () => (
     <BrowserRouter>
         <Routes>
-            <Route path='/' element={<div> Start page</div>} />
+            <Route path='/' element={<StartPage />} />
             <Route path='/form' element={<div> Form</div>} />
             <Route path='/final' element={<div> Final page</div>} />
             <Route path='*' element={<div> Not Found</div>} />
