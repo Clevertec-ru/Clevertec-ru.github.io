8540206614359167fde02aa585aa89576924d689
Aliaksei Olesik <AOlesik@alfabank.ru>
2024-11-02 17:43:15 +0300

    feat: add layout for confirm modals

--- src/components/modals/gosuslug-modal/gosuslug-modal.tsx
+++ src/components/modals/gosuslug-modal/gosuslug-modal.tsx
@@ -5,7 +5,11 @@ import { Button } from '@alfalab/core-components/button';
 import { ModalNames, modalsSelector, setModalOpen } from '~/redux/slices/modals.ts';
 import { useNavigate } from 'react-router-dom';
 import { Roots } from '~/types/roots.ts';
+import { Space } from '@alfalab/core-components/space';
+import { Typography } from '@alfalab/core-components/typography';
 
+import s from '../confirm-modals.module.css';
+import { GenericWrapper } from '@alfalab/core-components/generic-wrapper';
 export const GosuslugModal = () => {
     const navigate = useNavigate();
     const { GOSUSLUG } = useSelector(modalsSelector);
@@ -32,12 +36,18 @@ export const GosuslugModal = () => {
 
     return (
         <Modal open={GOSUSLUG}>
-            <Modal.Content>Окно на стороне Госуслуг с согласием на условия</Modal.Content>
-            <Modal.Footer>
-                Согласие получено
-                <Button onClick={handleConfirm}>Да</Button>
-                <Button onClick={handleCancel}>Нет</Button>
-            </Modal.Footer>
+            <Modal.Content className={s.content}>
+                <Typography.Text tag={'div'} weight={'bold'} className={s.contentText}>
+                    Окно на стороне Госуслуг с согласием на условия
+                </Typography.Text>
+                <Typography.Text className={s.contentText} tag={'div'}>
+                    Согласие получено?
+                </Typography.Text>
+                <GenericWrapper grow={true} gap={16} justifyContent={'center'}>
+                    <Button onClick={handleConfirm}>Да</Button>
+                    <Button onClick={handleCancel}>Нет</Button>
+                </GenericWrapper>
+            </Modal.Content>
         </Modal>
     );
 };
--- src/components/modals/payment-frame-modal/payment-frame-modal.tsx
+++ src/components/modals/payment-frame-modal/payment-frame-modal.tsx
@@ -4,6 +4,9 @@ import { Button } from '@alfalab/core-components/button';
 import { ModalNames, modalsSelector, setModalOpen } from '~/redux/slices/modals.ts';
 import { useNavigate } from 'react-router-dom';
 import { Roots } from '~/types/roots.ts';
+import { Typography } from '@alfalab/core-components/typography';
+import s from '~/components/modals/confirm-modals.module.css';
+import { GenericWrapper } from '@alfalab/core-components/generic-wrapper';
 
 export const PaymentFrameModal = () => {
     const { PAYMENT_FRAME } = useSelector(modalsSelector);
@@ -32,14 +35,18 @@ export const PaymentFrameModal = () => {
 
     return (
         <Modal open={PAYMENT_FRAME}>
-            <Modal.Content>
-                Системное окно на стороне {isEquiring ? 'эквайринга' : 'СБП'}
+            <Modal.Content className={s.content}>
+                <Typography.Text className={s.contentText} tag={'div'} weight={'bold'}>
+                    Системное окно на стороне {isEquiring ? 'эквайринга' : 'СБП'}
+                </Typography.Text>
+                <Typography.Text className={s.contentText} tag={'div'}>
+                    Оплата прошла успешно?
+                </Typography.Text>
+                <GenericWrapper grow={true} gap={16} justifyContent={'center'}>
+                    <Button onClick={handleOk}>Да</Button>
+                    <Button onClick={handleCancel}>Нет</Button>
+                </GenericWrapper>
             </Modal.Content>
-            <Modal.Footer>
-                Оплата прошла успешно?
-                <Button onClick={handleOk}>Да</Button>
-                <Button onClick={handleCancel}>Нет</Button>
-            </Modal.Footer>
         </Modal>
     );
 };
