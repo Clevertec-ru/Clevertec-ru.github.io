4fa190261493020987520798585a8b9a50f480eb
valadzkoaliaksei <avolodko@alfabank.ru>
2024-11-02 23:53:47 +0300

    fix(*): vite3

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import commonjs from '@rollup/plugin-commonjs';
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [commonjs(), react()],
+    plugins: [react()],
     server: {
         host: true,
         port: 3000,
@@ -14,5 +13,8 @@ export default defineConfig({
         alias: {
             '~': path.resolve(__dirname, 'src'),
         },
+    },
+    build: {
+        commonjsOptions: { transformMixedEsModules: true },
     },
 });
