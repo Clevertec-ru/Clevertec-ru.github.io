7c7f573926ada250a66c5ab38f952dd4dd7748f1
Aliaksei Olesik <AOlesik@alfabank.ru>
2024-11-02 16:34:29 +0300

    feat: add base layout for input method modal

---
+++ src/components/modals/custom-close.tsx
@@ -0,0 +1,12 @@
+import { NoShape } from '@alfalab/core-components/icon-view/components';
+import closeIcon from '../../assets/Cross.svg';
+
+import styles from './custom-close.module.css';
+
+export const CustomClose = () => {
+    return (
+        <NoShape size={32} iconContainerClassName={styles.shape}>
+            <img src={closeIcon} alt={'close-modal-icon'} />
+        </NoShape>
+    );
+};
--- src/components/modals/info-input-method-modal/info-input-method-modal.tsx
+++ src/components/modals/info-input-method-modal/info-input-method-modal.tsx
@@ -2,6 +2,13 @@ import { Modal } from '@alfalab/core-components/modal';
 import { Button } from '@alfalab/core-components/button';
 import { useDispatch, useSelector } from 'react-redux';
 import { ModalNames, modalsSelector, setModalOpen } from '~/redux/slices/modals.ts';
+import { CustomClose } from '~/components/modals/custom-close.tsx';
+import icon from '../../../assets/Cross.svg';
+import gosLogo from '../../../assets/gos_logo.svg';
+
+import styles from '../custom-close.module.css';
+import s from './info-input-method-modal.module.css';
+import { Space } from '@alfalab/core-components/space';
 
 export const InfoInputMethodModal = () => {
     const { INPUT_METHOD } = useSelector(modalsSelector);
@@ -25,14 +32,40 @@ export const InfoInputMethodModal = () => {
 
     return (
         <Modal open={INPUT_METHOD} onClose={onCancel}>
-            <Modal.Header hasCloser={true}>Выберите способ внесения данных</Modal.Header>
-            <Modal.Content>
+            <Modal.Header
+                hasCloser={true}
+                // closerIcon={<CustomClose />}
+                closerIcon={<img src={icon} />}
+                closerClassName={styles.closer}
+                contentClassName={s.content}
+                className={s.header}
+            >
+                Выберите способ внесения данных
+            </Modal.Header>
+            <Modal.Content className={s.modalContent}>
                 <p>
                     Вы можете указать информацию, необходимую для покупки полиса, самостоятельно или
                     использовать эккаунт на госуслугах для автоматического заполнения полей
                 </p>
-                <Button onClick={handleClick}>Заполнить самостоятельно</Button>
-                <Button onClick={handleClick}>Заполнить через Госуслуги</Button>
+                <Space direction='vertical' size={25} fullWidth={true}>
+                    <Button
+                        onClick={handleClick}
+                        block={true}
+                        view={'outlined'}
+                        className={s.baseBtn}
+                    >
+                        Заполнить самостоятельно
+                    </Button>
+                    <Button
+                        onClick={handleClick}
+                        block={true}
+                        className={s.blueBtn}
+                        view={'outlined'}
+                        leftAddons={<img src={gosLogo} />}
+                    >
+                        Заполнить через Госуслуги
+                    </Button>
+                </Space>
             </Modal.Content>
         </Modal>
     );
