853badb4216c841e060994c76a777694ad7e09d2
valadzkoaliaksei <avolodko@alfabank.ru>
2024-11-02 23:42:42 +0300

    fix(*): vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import commonjs from '@rollup/plugin-commonjs';
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [commonjs(), react()],
     server: {
         host: true,
         port: 3000,
