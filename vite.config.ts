91ff0dfbd54d1c81f55c45038e35217fa7e8bb5a
Иванова Татьяна Игоревна <TIIvanova@alfabank.ru>
2024-11-03 16:22:47 +0300

    feat(*): add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,31 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'РОСГОССТРАХ ЖИЗНЬ',
+                short_name: 'СПОРТ-ГАРАНТ',
+                start_url: '/',
+                display: 'standalone',
+                background_color: '#ffffff',
+                theme_color: '#990032',
+                icons: [
+                    {
+                        src: 'favicon.ico',
+                        sizes: 'any',
+                        type: 'image/x-icon',
+                    },
+                ],
+            },
+        }),
+    ],
     server: {
         host: true,
         port: 3000,
